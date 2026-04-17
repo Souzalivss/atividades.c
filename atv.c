2dba20fe97a9e1ad884db7ab01136b876863321c
Souzalivss <souzalivia8412@gmail.com>
2026-04-17 11:02:59 -0300

    decisoesAninhadas

--- atv.c
+++ atv.c
@@ -1,2 +1,31 @@
-//teste
+#include <stdio.h>
+
+int main () {
+    int idade, dependentes;
+    float renda;
+
+    printf("digite a sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite a sua renda mensal: \n");
+    scanf("%f", &renda);
+    printf("Digite o numero de dependentes\n");
+    scanf("%d", &dependentes);
+
+    if(idade >= 18 && idade <= 65){
+        if( renda < 3000) {
+            if(dependentes > 2) {
+                printf("Voce está qualificado para os benefícios");
+            } else {
+        printf("voce nao está qualificado devido ao numero de dependentes");
+    }
+        } else {
+            printf("voce nao esta qualificado devido à renda");
+        }
+    } else {
+        printf("voce nao está qualificado devido à idade");
+    }
+
+
+    
+}
 
